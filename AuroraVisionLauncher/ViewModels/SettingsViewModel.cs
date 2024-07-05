9b5ef6209aec692dd3a274e1eade85f6e7729c51
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-07-05 22:44:19 +0200

    used relaycommand in settings

--- AuroraVisionLauncher/ViewModels/SettingsViewModel.cs
+++ AuroraVisionLauncher/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 using AuroraVisionLauncher.Contracts.Services;
 using AuroraVisionLauncher.Contracts.ViewModels;
@@ -12,16 +13,14 @@ using Microsoft.Extensions.Options;
 namespace AuroraVisionLauncher.ViewModels;
 
 // TODO: Change the URL for your privacy policy in the appsettings.json file, currently set to https://YourPrivacyUrlGoesHere
-public class SettingsViewModel : ObservableObject, INavigationAware
+public partial class SettingsViewModel : ObservableObject, INavigationAware
 {
     private readonly AppConfig _appConfig;
     private readonly IThemeSelectorService _themeSelectorService;
     private readonly ISystemService _systemService;
     private readonly IApplicationInfoService _applicationInfoService;
     private AppTheme _theme;
-    private string _versionDescription;
-    private ICommand _setThemeCommand;
-    private ICommand _privacyStatementCommand;
+    private string _versionDescription = string.Empty;
 
     public AppTheme Theme
     {
@@ -35,10 +34,6 @@ public class SettingsViewModel : ObservableObject, INavigationAware
         set { SetProperty(ref _versionDescription, value); }
     }
 
-    public ICommand SetThemeCommand => _setThemeCommand ?? (_setThemeCommand = new RelayCommand<string>(OnSetTheme));
-
-    public ICommand PrivacyStatementCommand => _privacyStatementCommand ?? (_privacyStatementCommand = new RelayCommand(OnPrivacyStatement));
-
     public SettingsViewModel(IOptions<AppConfig> appConfig, IThemeSelectorService themeSelectorService, ISystemService systemService, IApplicationInfoService applicationInfoService)
     {
         _appConfig = appConfig.Value;
@@ -47,22 +42,25 @@ public class SettingsViewModel : ObservableObject, INavigationAware
         _applicationInfoService = applicationInfoService;
     }
 
+    [RelayCommand]
     public void OnNavigatedTo(object parameter)
     {
         VersionDescription = $"{Properties.Resources.AppDisplayName} - {_applicationInfoService.GetVersion()}";
         Theme = _themeSelectorService.GetCurrentTheme();
     }
 
+    [RelayCommand]
     public void OnNavigatedFrom()
     {
     }
-
+    [RelayCommand]
     private void OnSetTheme(string themeName)
     {
         var theme = (AppTheme)Enum.Parse(typeof(AppTheme), themeName);
         _themeSelectorService.SetTheme(theme);
     }
-
+    [RelayCommand]
     private void OnPrivacyStatement()
         => _systemService.OpenInWebBrowser(_appConfig.PrivacyStatement);
+
 }
