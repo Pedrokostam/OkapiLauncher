cd2674e874058bce0a17b30d83c5828b0e3561ab
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-10-02 23:05:21 +0200

    Pathstem documentation

--- AuroraVisionLauncher.Core/Models/Apps/Detection/PathStem.cs
+++ AuroraVisionLauncher.Core/Models/Apps/Detection/PathStem.cs
@@ -1,32 +1,53 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace AuroraVisionLauncher.Core.Models.Apps.Detection
 {
+    /// <summary>
+    /// Class which specifeid how an app path should look.
+    /// </summary>
     [DebuggerDisplay("{Filename}")]
     class PathStem
     {
-        private string[] _parts;
-        public ReadOnlySpan<string> Parts => (ReadOnlySpan<string>)_parts;
+        private readonly string[] _parts;
+        /// <summary>
+        /// Steps to the executable from the root folder of the app (exclusive) to the filename (inclusive)
+        /// </summary>
+        public IReadOnlyList<string> Parts => new ReadOnlyCollection<string>(_parts);
+        /// <summary>
+        /// The filename of the executable.
+        /// </summary>
         public string Filename => _parts[^1];
         public int MaxDepth => _parts.Length;
-        private string? _ending = null;
         /// <summary>
-        /// Folders which, if the path ends in them should be exited for their parent
+        /// Folders which, if the path ends in them should be exited for their parent. If the path to match ends at one of this, its parent will be analyzed instead. Order does not matter, unlike in <see cref="Parts"/>
         /// </summary>
         public string[] FoldersToLeave { get; init; } = [];
-        public string Ending
-        {
-            get
-            {
-                _ending ??= string.Join(Path.DirectorySeparatorChar, _parts).ToLowerInvariant();
-                return _ending;
-            }
-        }
+        /// <summary>
+        /// Combined <see cref="Parts"/> using the system separator and lowercased.
+        /// </summary>
+        public string Ending { get; set; }
+
         public ReadOnlySpan<string> Preceding => new(_parts, 0, _parts.Length - 1);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parts">Steps to the executable from the root folder of the app (exclusive) to the filename (inclusive). Must have at least 1 element (filename).</param>
         public PathStem(params string[] parts)
         {
+            if (parts.Length == 0)
+            {
+                throw new ArgumentException("At least the filename must be specified");
+            }
             _parts = parts;
+            Ending = string.Join(Path.DirectorySeparatorChar, _parts).ToLowerInvariant();
         }
+        /// <summary>
+        /// Checks whether the given filepath matches. The filepath will match if it ends at any of the parts or the root folder of the app.
+        /// </summary>
+        /// <param name="path">Path in which executable should be stored.</param>
+        /// <returns>Path to the executable or null</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public string? MatchPathInfo(string path)
         {
             if (File.Exists(path))
@@ -41,7 +62,9 @@ namespace AuroraVisionLauncher.Core.Models.Apps.Detection
                 {
                     path = Path.GetDirectoryName(path)!;
                     if (path is null)
-                    { throw new InvalidOperationException($"Infinite loop at avoiding folders for {path}"); }
+                    {
+                        throw new InvalidOperationException($"Infinite loop at avoiding folders for {path}");
+                    }
                 }
             }
             ArgumentNullException.ThrowIfNull(path);
