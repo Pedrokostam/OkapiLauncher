2bff0d14cbea510fe90526427d4ab6c0479aec02
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-10-15 19:02:17 +0200

    Created a script for publishing and zipping

--- build.ps1
+++
@@ -1,9 +0,0 @@
-Push-Location $PSScriptRoot
-try {
-    $ProjPath = "AuroraVisionLauncher/AuroraVisionLauncher.csproj"
-    $PubFilePath = "AuroraVisionLauncher/AuroraVisionLauncher/Properties/PublishProfiles/FolderProfile.pubxml"
-    dotnet publish $ProjPath -c Release  "/p:PublishProfile=$PubFilePath"
-}finally{
-
-}
-Pop-Location
